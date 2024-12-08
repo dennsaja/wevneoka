7a0ab0ee7206e11cd40cb9cdff8548e09ff3b4f8
Denis <136689391+dennsaja@users.noreply.github.com>
2024-12-08 17:56:11 +0700

    Update and rename contact.js to chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get("/", async (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'contact.html'));
+  res.sendFile(path.join(__dirname, '..', 'views', 'chat.html'));
 });
 
 module.exports = router;
